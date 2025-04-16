e1b78f5b7eb2a777a2c629cada012eff0c71870f
sfiller23 <sf27lp@gmail.com>
2025-04-16 13:55:33 +0300

    cors origin update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
@@ -15,6 +16,13 @@ mongoose
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
+// Allow requests from http://localhost:5173
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
